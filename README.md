c045bcca0c2cfab75c9be27f244cd54b93a965ca
Ben Nemec <bnemec@redhat.com>
2021-12-16 12:28:04 -0600

    Add script to automate deployment
    The script serves two purposes: First, it automates all of the
    commands that were previously documented in the readme. Second, it
    automates updating of the image in the pod definitions. It's not
    possible to look up the image location from the config map directly,
    but we can look at the config map YAML from the script and extract
    the image location that way. This makes user_config.yaml the only
    place users will need to modify values.

--- README.md
+++ README.md
@@ -1,11 +1,4 @@
 # Deployment instructions
 
-Run the following commands:
-
-- oc apply -f namespace.yaml
 - Edit user_config.yaml file
-- oc apply -f user_config.yaml
-- oc apply -f scc.yaml
-- oc adm policy add-scc-to-user privileged -n keepalived-v6  -z cluster-hosted-keepalived
-- oc apply -f keepalived_config.yaml
-- oc apply -f keepalived_daemonset.yaml
+- Run deploy.sh
---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+# The image can't be looked up from the config map, so we have to manually
+# insert it into the daemonset spec.
+image="$(yq -r .data.keepalived_image_name user_config.yaml)"
+sed -e "s|@KEEPALIVED_IMAGE|$image|" keepalived_daemonset.tmpl > keepalived_daemonset.yaml
+
+oc apply -f ./namespace.yaml
+oc apply -f ./user_config.yaml
+oc apply -f ./scc.yaml
+oc adm policy add-scc-to-user privileged -n keepalived-v6 -z cluster-hosted-keepalived
+oc apply -f keepalived_config.yaml
+oc apply -f keepalived_daemonset.yaml
